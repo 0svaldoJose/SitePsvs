9e6bfde960476e3e4165924afc73ab590c784a85
OsvaldoJose <OsvaldoJo2004@gmail.com>
2022-11-09 14:50:32 -0300

    09/11

---
+++ horizon.js
@@ -0,0 +1,37 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 5,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        500: {
+            slidesPerView: 2,
+        },
+      
+        1013.3: {
+          slidesPerView: 3,
+        },
+        1223:{
+          slidesPerView: 4,
+        },
+        
+        1600: {
+          slidesPerView: 5,
+        }
+    }
+  });
